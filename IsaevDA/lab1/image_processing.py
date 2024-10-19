57c971e4740ec984d9e77e9108820f2e73010935
4isper <dima44162@yandex.ru>
2024-10-19 13:32:16 +0300

    some improvements

--- IsaevDA/lab1/image_processing.py
+++ IsaevDA/lab1/image_processing.py
@@ -4,23 +4,29 @@ import cv2 as cv
 import os
 import numpy as np
 
+rect_start = (0, 0)
+rect_end = (0, 0)
+drawing = False
+
 def parse_arguments():
   parser = argparse.ArgumentParser()
 
   parser.add_argument('-m', '--mode', help='Choose mode (image/imgproc)', dest='mode', default='image')
   parser.add_argument('-f', '--func', choices=['grayscale', 'resize', 'sepia', 'vignette', 'pixelation'],dest='func')
   parser.add_argument('-i', '--image',type=str,help='Path to input image', dest='image_path')
-  parser.add_argument('-o', '--output', type=str, help='Output image name', dest='output_image',default='image_out.jpg',)
+  parser.add_argument('-o', '--output', type=str, help='Output image name', dest='output_image',default='image_out.jpg')
+
+  parser.add_argument('-v', '--vignette-intensity', type=float, help='Intensity of vignette 0 to 1 (not inclusive)', dest='vignette_intensity', default=0.15)
+  parser.add_argument('-r', '--vignette-radius', type=float, help='Radius of vignette circle (0 to 1, where 1 is max)', dest='vignette_radius', default=0.5)
+  parser.add_argument('-s', '--scale', type=float, dest='scale', default=0.5)
+  parser.add_argument('-p', '--pixel-size', type=int, help='Block size for pixelation effect', dest='pixel_size', default=30)
 
   args = parser.parse_args()
   return args
 
-def highgui_samples(image_path):
-  img = cv.imread(image_path)
 
-  if img is None:
-    print("Could not open or find the image")
-    return
+def highgui_samples(image_path):
+  img = read_image(image_path)
 
   cv.imshow('Init image', img)
   while True:
@@ -30,6 +36,7 @@ def highgui_samples(image_path):
       break
 
   cv.destroyAllWindows()
+
 
 def generate_unique_filename(filename):
   base, ext = os.path.splitext(filename)
@@ -41,20 +48,19 @@ def generate_unique_filename(filename):
     
   return filename
 
-# Функция перевода изображения в оттенки серого
-def grayscale(image_path, output_image):
+
+def read_image(image_path):
   img = cv.imread(image_path)
 
   if img is None:
     print("Could not open or find the image")
     return
+  
+  return img
 
-  height, width, n = img.shape
-  for i in range(height):
-    for j in range(width):
-      img[i,j] = 0.3*img[i,j,2] + 0.59*img[i,j,1] + 0.11*img[i,j,0]
 
-  cv.imshow('Gray image', img)
+def show_end_image(img, output_image):
+  cv.imshow('Processed Image', img)
   output_image = generate_unique_filename(output_image)
   cv.imwrite(output_image, img)
   
@@ -66,144 +72,77 @@ def grayscale(image_path, output_image):
   
   cv.destroyAllWindows()
 
+# Функция перевода изображения в оттенки серого
+def grayscale(img):
+  gray_img = np.zeros_like(img,np.uint8)
+
+  gray_img[:,:,0] = 0.3*img[:,:,2] + 0.59*img[:,:,1] + 0.11*img[:,:,0]
+  gray_img[:,:,1] = gray_img[:,:,0]
+  gray_img[:,:,2] = gray_img[:,:,0]
+
+  return gray_img
+
 # Функция изменения разрешения изображения
-def resize(image_path, output_image):
-  img = cv.imread(image_path)
-
-  if img is None:
-    print("Could not open or find the image")
-    return
-
+def resize(img, scale):
   height, width, n = img.shape
-  scaleX, scaleY = 2, 2
 
-  resize_img = np.zeros((height//scaleX, width//scaleY, n),np.uint8)
+  new_height = int(height * scale)
+  new_width = int(width * scale)
 
-  B = img[:,:,0]
-  G = img[:,:,1]
-  R = img[:,:,2]
+  # Создаем сетки индексов для нового изображения
+  row_indices = (np.arange(new_height) / scale).astype(int)
+  col_indices = (np.arange(new_width) / scale).astype(int)
 
-  resizeB = B[1::scaleX,1::scaleY]
-  resizeG = G[1::scaleX,1::scaleY]
-  resizeR = R[1::scaleX,1::scaleY]
+  row_indices = np.clip(row_indices, 0, height - 1)
+  col_indices = np.clip(col_indices, 0, width - 1)
 
-  resize_img[:,:,0] = resizeB
-  resize_img[:,:,1] = resizeG
-  resize_img[:,:,2] = resizeR
-
-  cv.imshow('Resize image', resize_img)
-  output_image = generate_unique_filename(output_image)
-  cv.imwrite(output_image, resize_img)
+  # Применяем индексы для создания нового изображения
+  resize_img = img[row_indices[:, np.newaxis], col_indices]
 
   print(f'Original shape: {img.shape}')
   print(f'Resized shape: {resize_img.shape}')
 
-  while True:
-    key = cv.waitKey(0)
-    if key == 27:  # Код клавиши Esc
-      print("Escape pressed, closing window.")
-      break
-
-  cv.destroyAllWindows()
+  return resize_img
 
 # Функция применения фотоэффекта сепии к изображению
-def sepia(image_path, output_image):
-  img = cv.imread(image_path)
+def sepia(img):
+  sepia_img = np.zeros_like(img, np.uint8)
 
-  if img is None:
-    print("Could not open or find the image")
-    return
+  sepia_img[:,:,2] = np.clip(0.393*img[:,:,2] + 0.769*img[:,:,1] + 0.189*img[:,:,0], 0, 255)
+  sepia_img[:,:,1] = np.clip(0.349*img[:,:,2] + 0.686*img[:,:,1] + 0.168*img[:,:,0], 0, 255)
+  sepia_img[:,:,0] = np.clip(0.272*img[:,:,2] + 0.534*img[:,:,1] + 0.131*img[:,:,0], 0, 255)
 
-  height, width = img.shape[:2]
-  for i in range(height):
-    for j in range(width):
-      newR = int(0.393*img[i,j,2] + 0.769*img[i,j,1] + 0.189*img[i,j,0])
-      newG = int(0.349*img[i,j,2] + 0.686*img[i,j,1] + 0.168*img[i,j,0])
-      newB = int(0.272*img[i,j,2] + 0.534*img[i,j,1] + 0.131*img[i,j,0])
+  return sepia_img
 
-      if newR > 255:
-        newR = 255
-      if newG > 255:
-        newG = 255
-      if newB > 255:
-        newB = 255
 
-      img[i,j,2] = newR
-      img[i,j,1] = newG
-      img[i,j,0] = newB
+def create_vignette_mask(height, width, intensity, radius):
+  y, x = np.ogrid[:height, :width]
+  center_x = width // 2
+  center_y = height // 2
 
-  cv.imshow('Sepia image', img)
-  output_image = generate_unique_filename(output_image)
-  cv.imwrite(output_image, img)
+  max_radius = min(center_x, center_y) * radius
+  distance = np.sqrt((x - center_x) ** 2 + (y - center_y) ** 2)
 
-  while True:
-    key = cv.waitKey(0)
-    if key == 27:  # Код клавиши Esc
-      print("Escape pressed, closing window.")
-      break
-
-  cv.destroyAllWindows()
-
-def create_vignette_mask(height, width, sigma):
-  centerX = width // 2
-  centerY = height // 2
-    
-  mask = np.zeros((height, width), dtype=np.float32)
-
-  for i in range(height):
-    for j in range(width):
-      dx = (j - centerX) ** 2
-      dy = (i - centerY) ** 2
-      distance = np.sqrt(dx + dy)
-
-      mask[i, j] = np.exp(-(distance ** 2) / ( sigma ** 2)) # гауссово значение
-
+  mask = np.exp(-((distance / max_radius) ** 2) * intensity)
   mask = mask / np.max(mask)
 
   return mask
 
 # Функция применения фотоэффекта виньетки к изображению
-def vignette(image_path, output_image):
-  img = cv.imread(image_path)
-
-  if img is None:
-    print("Could not open or find the image")
-    return
-
+def vignette(img, vignette_intensity, vignette_radius):
   height, width, n = img.shape
-  mask = create_vignette_mask(height, width, 800)
 
-  for i in range(n):
-    img[:,:,i] = img[:,:,i] * mask
+  mask = create_vignette_mask(height, width, vignette_intensity, vignette_radius)
 
-  cv.imshow('Image with a vignette', img)
-  output_image = generate_unique_filename(output_image)
-  cv.imwrite(output_image, img)
+  img = img * mask[:, :, np.newaxis]
+  img = img.astype(np.uint8)
 
-  while True:
-    key = cv.waitKey(0)
-    if key == 27:  # Код клавиши Esc
-      print("Escape pressed, closing window.")
-      break
-
-  cv.destroyAllWindows()
+  return img
 
 # Функция пикселизации заданной прямоугольной области изображения
-def pixelation(image_path, output_image):
-  img = cv.imread(image_path)
-
-  if img is None:
-    print("Could not open or find the image")
-    return
-
-  # Определяем координаты прямоугольной области (пикселизируем центральную область)
-  height, width = img.shape[:2]
-  top_left = (width // 4, height // 4)  # Верхний левый угол
-  bottom_right = (3 * width // 4, 3 * height // 4)  # Нижний правый угол
-
-  x1, y1 = top_left
-  x2, y2 = bottom_right
-  block_size = 30
+def pixelation(img, rect_start, rect_end, block_size):
+  x1, y1 = rect_start
+  x2, y2 = rect_end
 
   output = img.copy()
 
@@ -211,22 +150,45 @@ def pixelation(image_path, output_image):
     for j in range(x1, x2, block_size):
       block_y2 = min(i + block_size, y2)
       block_x2 = min(j + block_size, x2)
-
       block = output[i:block_y2, j:block_x2]
       avg_color = np.mean(block, axis=(0, 1), dtype=int)
       output[i:block_y2, j:block_x2] = avg_color
 
-  cv.imshow('Image with a pixelation aria', output)
-  output_image = generate_unique_filename(output_image)
-  cv.imwrite(output_image, output)
+  return output
+
+# Обработчик событий мыши для выбора области
+def mouse_callback(event, x, y, flags, param):
+  global rect_start, rect_end, drawing
+
+  if event == cv.EVENT_LBUTTONDOWN:
+    rect_start = (x, y)
+    drawing = True
+
+  elif event == cv.EVENT_MOUSEMOVE:
+    if drawing:
+      img_copy = param.copy()
+      cv.rectangle(img_copy, rect_start, (x, y), (0, 255, 0), 2)
+      cv.imshow('Image', img_copy)
+
+  elif event == cv.EVENT_LBUTTONUP:
+    rect_end = (x, y)
+    drawing = False
+
+
+def select_pixelation_area(img):
+  global rect_start, rect_end
+
+  cv.imshow('Image', img)
+  cv.setMouseCallback('Image', mouse_callback, img)
 
   while True:
-    key = cv.waitKey(0)
-    if key == 27:  # Код клавиши Esc
-      print("Escape pressed, closing window.")
+    key = cv.waitKey(10) & 0xFF
+    if key == 13:  # Нажатие Enter для подтверждения
       break
 
   cv.destroyAllWindows()
+  return rect_start, rect_end
+
 
 def main():
   args = parse_arguments()
@@ -234,16 +196,33 @@ def main():
   if args.mode == 'image':
     highgui_samples(args.image_path)
   elif args.mode == 'imgproc':
+    img = read_image(args.image_path)
+
     if args.func == 'grayscale':
-      grayscale(args.image_path, args.output_image)
+      img = grayscale(img)
     elif args.func =='resize':
-      resize(args.image_path, args.output_image)
+      if args.scale <= 0:
+        print("Значение должно быть больше 0!")
+        return
+      img = resize(img, args.scale)
     elif args.func =='sepia':
-      sepia(args.image_path, args.output_image)
+      img = sepia(img)
     elif args.func == 'vignette':
-      vignette(args.image_path, args.output_image)
+      if args.vignette_intensity <= 0 or args.vignette_intensity >= 1:
+        print("Интенсивность виньетки должна быть между 0 и 1 (не включительно)!")
+        return
+      if args.vignette_radius <= 0 or args.vignette_radius > 1:
+        print("Радиус виньетки должен быть между 0 и 1!")
+        return
+      img = vignette(img, args.vignette_intensity, args.vignette_radius)
     elif args.func == 'pixelation':
-      pixelation(args.image_path, args.output_image)
+      if args.pixel_size <= 1:
+        print("Значение должно быть больше 1!")
+        return
+      rect_start, rect_end = select_pixelation_area(img)
+      img = pixelation(img, rect_start, rect_end, args.pixel_size)
+
+    show_end_image(img, args.output_image)
   else:
     raise 'Unsupported \'mode\' value'
 
